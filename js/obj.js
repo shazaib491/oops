a5d1debd6e69b348754f54c12558ea4d6db686e5
shazaib491 <shazaib491@github.com>
2022-05-11 23:31:34 +0530

    latest

--- js/obj.js
+++ js/obj.js
@@ -1,9 +1,37 @@
-// object is key value pair
+// MERN STACK
+// M => MOGNODB =>  DATABASE
+// E => EXPRESSJS => Framework
+// R => REACTJS => Library frontend 
+// N => Nodejs => RunTime Environment of js
 
-// akash choudry
-const name="akash"
-const age=18
-const cls=12
-const salary=10000000
 
-console.log(name,age,cls,salary)
+// CLIENT SITE OR SERVER SITE
+
+//45 DAYS
+
+// html,css,bootsrap,js===>reactjs nodejs expresssjs mongodb
+
+
+// Library vs Framework
+// collection  of function
+
+
+// REQUEST =>DOMAIN =>
+// SERVER WEBSITE  UPLOAD 
+
+// html
+// hypertext markup langugae 
+// it's used to design our webpage structure 
+
+// a collection of webpage is called website
+// two type
+// static page dynamic page 
+
+// xml ==>extensible markup language  => element manually create 
+// html => predefined element
+
+// .html .htm
+// pagename.html pagename.htm
+
+
+
