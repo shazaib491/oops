fc38f3c5aa15b054608854eb7ef846c9712e6f44
shazaib491 <shazaib491@github.com>
2022-05-12 20:36:57 +0530

    finalCommit

--- js/obj.js
+++ js/obj.js
@@ -1,37 +1,108 @@
-// MERN STACK
-// M => MOGNODB =>  DATABASE
-// E => EXPRESSJS => Framework
-// R => REACTJS => Library frontend 
-// N => Nodejs => RunTime Environment of js
+// // 
+// let name="akashj"
+// let cls="12th"
+// let subject="english"
+// let marks=100;
+
+// // object literal
+// // object is a collection key and value pair
+// let obj={
+//     name:"admin",
+//     cls:12,
+//     english:100,
+//     hindi:80,
+//     social:90
+// }
 
 
-// CLIENT SITE OR SERVER SITE
-
-//45 DAYS
-
-// html,css,bootsrap,js===>reactjs nodejs expresssjs mongodb
+// obj.firstname=obj.name;
+// delete obj.name;
+// // console.log(obj)
 
 
-// Library vs Framework
-// collection  of function
+// // let renameKey=(keyMap,object)=>{
+// //     Object.keys(object).reduce((acc,key)=>({
+// //         ...acc,
+// //         ...{ [keyMap[key] ]: object[key] },
+// //     })),
+// //     {}
+// // }
+
+    
 
 
-// REQUEST =>DOMAIN =>
-// SERVER WEBSITE  UPLOAD 
+// let renameKeys = (keysMap, object) =>
+// Object.keys(object).reduce(
+//   (acc, key) => ({
+//     ...acc,
+//     ...{ [keysMap[key] || key]: object[key] },
+//   }),
+  
+//   {}
+// );
 
-// html
-// hypertext markup langugae 
-// it's used to design our webpage structure 
+// let result=renameKeys({name:"FirstName"},obj)
+// console.log(result)
 
-// a collection of webpage is called website
-// two type
-// static page dynamic page 
+// // // console.log(obj['name'])
+// // for(let i in obj){
+// //     console.log(obj[i])
+// // }
 
-// xml ==>extensible markup language  => element manually create 
-// html => predefined element
+// const students=[
+//     {
+//         name:"admin",
+//         cls:"12th",
+//         english:100
+//     },
+//     {
+//         name:"shazaib",
+//         cls:"12th",
+//         english:100
+//     },
+//     {
+//         name:"akash",
+//         cls:"12th",
+//         english:100
+//     },
+//     {
+//         name:"shika",
+//         cls:"12th",
+//         english:100
+//     },
+//     {
+//         name:"roshni",
+//         cls:"12th",
+//         english:100
+//     }
 
-// .html .htm
-// pagename.html pagename.htm
+// ]
+
+
+// for(let i =0; i<students.length; i++){
+//     console.log(students[i]['english'])
+// }
+
+// students.forEach((element,index,staticCopy)=>{
+//     console.log(element.name)
+//     console.log(element.cls)
+//     console.log(element.english)
+// })
+
+
+
+// symbol in js 
+let stud={}
+let sym1=Symbol("joe");
+stud[sym1]=100;
+let sym2=Symbol("shazaib");
+stud[sym2]=200;
+
+console.log(stud['joe'])
+
+
+
+
 
 
 
