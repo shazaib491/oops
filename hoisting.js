3fe28b5424b314949a01dba9f7a27b09d1ad9d99
“shazaib491” <shazaibrehman127@gmail.com>
2022-06-16 10:49:54 +0530

    latest

--- hoisting.js
+++ hoisting.js
@@ -1,5 +1,5 @@
-// a=10;
-// console.log(a);
+// // // a=10;
+// // // console.log(a);
 
 
 
@@ -10,31 +10,308 @@
 
 
 
-//1 var a;
-// // defined 
-// var a=10;
-// // refined
-// var a=20;
-// console.log(a);
+// // //1 var a;
+// // // // defined 
+// // // var a=10;
+// // // // refined
+// // // var a=20;
+// // // console.log(a);
 
-// var  name="admin";
-//     name="shazaib"; //overwrite
+// // // var  name="admin";
+// // //     name="shazaib"; //overwrite
 
-// global scope
-// local  scope
+// // // global scope
+// // // local  scope
 
-// block app
-// {
-//     var fname="afaque";
-//     console.log(fname)
+// // // block app
+// // // {
+// // //     var fname="afaque";
+// // //     console.log(fname)
 
-// }
+// // // }
 
 
-// console.log(fname)
+// // // console.log(fname)
 
-// for(var i=0; i<=5; i++){
-//     console.log(i)
-// }
+// // // for(var i=0; i<=5; i++){
+// // //     console.log(i)
+// // // }
 
-// console.log(i)
+// // // console.log(i)
+
+
+
+
+
+
+
+// // // var variablename="admin"
+// // // 1000='admin'
+
+
+// // var no = 100;
+
+// // var flag = 1;
+
+
+// // var decimal = 100.98;
+
+// // // wap to assign any two no find the sum
+// // // var fno=10;
+// // // var sno=20;
+// // // // value
+// // // var s=fno+sno;
+// // // console.log(s);
+
+// // // console.log(11%2);
+// // // // 2)11(5
+// // // //   10
+// // // //   --
+
+// // // // 2x2x2x2x2
+
+// // // var no=10;
+
+// // // no--;
+// // // console.log(no)
+
+// // // var x=10;
+// // // var y=20;
+
+// // // // x=x+y;
+// // // x*=y;
+// // // console.log(x)
+// // //     // 10+20;
+
+// // // x=x*y;
+
+// // // const vowel=prompt("Enter any character")
+
+// // // if((vowel=='a' || vowel=='e' || vowel=='i'  || vowel=='o' || vowel=='u') ||  (vowel=='A' || vowel=='E' || vowel=='I'  || vowel=='O' || vowel=='U'))
+// // //     console.log("character is vowel")
+// // // else
+// // //     console.log("characrter is not vowel")
+
+// // //nestedif
+
+
+// // // var a=11;
+
+// // // if(a==11)//true
+// // //     if(a%2==0) //false
+// // //         console.log("a is equal to 1q and a is even")
+// // //     else
+// // //         console.log("a is equal to 11 and a is odd")
+// // // else
+// // // console.log("a is not equal to 11");
+
+
+// // // 2)11(5
+// // //   10
+// // //   --
+// // //   1
+
+// // // wap to assign a name check string is not empty  and length >=5
+// // // name="admin"
+
+// // username = ""
+// // //   01234567891011  
+// // // console.log(name.length)
+// // if (username.length != 0) {
+// //     if (username.length > 5) {
+// //         console.log("given string is valid")
+
+// //     }
+// //     else {
+// //         console.log("string is not empty but not greater than 5")
+// //     }
+// // } else {
+// //     console.log("string is empty")
+// // }
+
+
+
+// // // wap to input mobile no check if no is not empty then if no length is   equal to  13
+// // // then print its valid no else print invalid no 
+// // // mobile=8959883069
+// // // mobile no length equal 13 
+// // // valid no else invalid no 
+
+
+
+// // // wap to input any no check if no is even then calculate exponention of no then check if no > 100
+// // // print no is valid else invalid 
+
+
+// // // wap to input any no check wehetr the no is single digit double digit or triple digit
+// // // 1 10
+
+
+// // // condition1 condition2
+
+// // // wap to assign three no print the greatest no bw them
+// // // var a=10;
+// // // var b=20; 
+// // // var c=30;
+
+
+// // // if(a > b && a>c){
+// // //     console.log(" a is greater no")
+// // // }else if (b > a && b > c){
+// // //     console.log("b is gretaerr no")
+// // // }else{
+// // //     console.log("c is greter no")
+// // // }
+
+// // // let number =+prompt("Enter Mobile no");
+// // // using nested if else
+// // // to check mobile sholud not be empty
+// // // if (mobileno.length >= 0) {
+// // //     if (mobileno.length == 12) {
+// // //         console.log("Valid mobile no")
+// // //     }
+// // //     else {
+// // //         console.log("Invalid Mobile no")
+// // //     }
+// // // }
+// // // else {
+// // //     console.log("Mobile no is Empty")
+
+// // // }
+
+// // // if(number%2==0){    
+// // //     let exp=number**number; 
+// // //     console.log(exp);
+// // //     console.log(exp > 100);
+// // //     if(exp > 100){
+// // //         console.log("Valid no")
+// // //     }else{
+// // //         console.log("not a valid no")
+// // //     }
+// // // }else{
+// // //     console.log("no is not even")
+// // // }
+
+// // // ladder if else
+
+// // let number=+prompt("Enter number")
+// // if(number==10){ //first condition false
+// //     console.log("no is 10")//statement 1
+// // }else if(number==40){//second condition false
+// //     console.log("no is 40")
+// // }else if(number==100){//second condition true
+// //     console.log("no is 100")
+// // }else{
+// //     console.log("no is not in range")
+// // }
+
+
+
+
+
+// // console.log("statement below if statement")
+
+
+// // let number=+prompt("Enter an number")
+// // if (number >100 ){
+// // console.log ("Greater than 100")
+// // } else if (number <100) {
+// //     console.log ("Less than 100")
+// // } else {
+// //     console.log ("equal to 100")
+// // }
+
+
+// // input
+// // let number=+prompt("Enter Number");
+// // switch(number){
+// //     case 1:
+// //         console.log("No is one");
+// //         break;
+// //     case 2:
+// //         console.log("No is two")
+// //         break;
+// //     case 3:
+// //         console.log("No is three")
+// //         break;
+// //     default:
+// //         console.log("Goto hell")
+// // }
+
+// // let day = new Date().getDay();
+// // let year = new Date().getFullYear();
+// // let hour = new Date().getMonth();
+// // console.log(`${day}/ ${hour} / ${year} `)
+// // console.log(new Date()))
+
+
+// // let date=new Date()
+
+// // let day=+prompt("Enter Dayh");
+
+// today='tuesday'
+
+
+// // switch(day) {
+// //     case 1:
+// //         console.log("Today is Monday")
+// //         break;
+// //     case 2:
+// //         console.log("Today is tuesday")
+// //         break;
+// //     case :
+// //         console.log("Today is wednesday")
+// //         break;
+// //     case 4:
+// //         console.log("Today is Thrusday")
+// //         break;
+// //     case 5:
+// //         console.log("Today is Friday")
+// //         break;
+// //     case 6:
+// //         console.log("Today is Saturday")
+// //         break;
+// //     default:
+// //         console.log("Today is Sunday")
+// //             ;
+// // }
+
+
+
+// // wap 
+
+
+
+
+
+//MERN STACK
+//MONGODB  ===> MONGODB ===>DATABASE
+//EXPRESSJS ===> NODEJS FRAMEWORK
+//REACTJS ===>LIBRARY
+//NODEJS  ==> JAVASCRIPT RUNTIME ENVRIONMENT
+
+// BASIC
+// HTML
+// CSS
+// BOOTSTRAP5
+// TEMPLATE
+// JAVASCRIPT
+
+
+
+
+// HTML for hypertext markup langugae
+// collection of webpage website
+//webpage html 
+// 
+
+
+// NOTEPAD
+// NOTEPAD++
+// VDCODE
+// ATOM
+// SUBLIME
+
+
+
+
---
+++ temporaldedzone.js
@@ -0,0 +1,131 @@
+// var a=10;
+// // defined
+// let  a=100;//overwrite 
+// // a is already declared
+// console.log(a)
+
+// let name="admin";
+// name="sara";
+// console.log(name);
+// console.log(a)
+// let a=10;
+
+// scope
+// {
+//     // local scope 
+//     let a=100;
+//     console.log(a)
+
+// }
+
+
+// for(let i=0; i<=10;i++){
+//     // console.log(i)
+// }
+// console.log(i)
+
+// const name="admin";
+// name="admin";
+// console.log(name)
+// console.clear();
+// {
+//   const a=10; 
+//   a=100;
+// console.log(a)
+
+// }
+
+//  for(const i=0; i<=100; i++){
+//      console.log(i)
+//  }
+
+
+// basic intro
+// data types
+// varbile
+// operator
+// hoisting
+// var problem
+// temporal deadzone es6 feature
+// control statement
+// decision making
+// syntax
+// if(condition) true
+    // console.log("success")
+
+// // else
+//     console.log("failure")
+
+
+
+
+
+
+
+
+// looping
+// (single if) (if else) 
+// (nested if else )
+// (ladder if else)
+// looping
+// while
+// do while
+// for loop
+// function 
+// (ps rs) (pn rn) (pn rs) (ps rn)
+// regular function
+
+// control + /
+/* 
+dffbsdknffd
+sd
+fsd
+fsd
+
+sdf
+sdfsd
+
+
+*/
+
+
+// wap to input any two no print the greatest no b/w them /
+// // input
+// let fno=+prompt("Enter first no");
+// let sno=+prompt("Enter first no");
+// if(fno > sno)
+//     console.log("greater no is ",fno)
+
+// // wap to input any no print only positive no
+// let pos=+prompt("Enter number");
+// if(pos > 0)
+// console.log("positive no is ",pos)//single statement 
+// console.log("Hello Wolrd")
+
+
+// let pos=+prompt("Enter number");
+// if(pos > 0)
+// console.log("positive no is ",pos)//single statement 
+// else
+// console.log("negative no is ",pos)
+// console.log("Hello Wolrd")
+
+// wap to check privilages if privilages role is admin then print your owner else print you are employee ?
+// admin ==> if =>true you are owner else => you are employee 
+let role=prompt("Enter role");
+if(role==='admin')
+    console.log("your are owner")
+else 
+    console.log("you are employee")
+
+// wap to input any no check wheter the no is even or odd 
+// 5 minute
+
+
+// program to find out whether a given year is a leap year or not
+// program to convert temperature from Fahrenheit to Celsius and Celsius to Fahrenheit
+// program to check whether a character is an alphabet or not
+// program to check vowel or consonant
+// program to enter student marks and find percentage and grade
+// program to check whether the triangle is valid or not if sides are given
+// program to check whether a number is a prime number or not
